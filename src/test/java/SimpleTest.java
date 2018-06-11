8da16a0cfcba86df1f5adb3650776932728e3a89
Viachaslau Amelin <Viachaslau_Amelin@epam.com>
2018-06-11 12:44:56 +0300

    commit for changesRepo

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -7,4 +7,9 @@ public class SimpleTest {
         System.out.println("Master branch greeting!");
     }
 
+    @Test
+    public void method2(){
+        System.out.println("ChangesRepo greeting!!!");
+    }
+
 }
